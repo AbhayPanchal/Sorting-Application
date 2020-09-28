0d41603091307e8bb88842661a04cc96d94f0d0c
AbhayPanchal <abhaypanchal32@gmail.com>
2020-09-28 17:05:33 -0400

    first commit

---
+++ Sorting Application/Assignment1_000813104/Employee.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+/// <summary>
+///  I, Abhay Panchal, 000813104 certify that this material is my original work.  
+///  No other person's work has been used without due acknowledgement.(SEP : 2020)
+/// </summary>
+
+namespace Assignment1_000813104
+{   
+    /// <summary>
+    /// Employee class here has many get and set methods which help the application to process the output for user.
+    /// Tostring method use to output the data perfectly on the console.
+    /// </summary>
+    class Employee
+    {
+        /// <summary>
+        /// Declaring the variables called name, number, rate, hours and gross
+        /// </summary>
+        
+        private string name;
+        private int number;
+        private decimal rate;
+        private double hours;
+
+        private decimal gross;
+
+        /// <summary>
+        /// Constructoe Employee for Class.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="number"></param>
+        /// <param name="rate"></param>
+        /// <param name="hours"></param>
+        public Employee(string name, int number, decimal rate, double hours)
+        {
+            this.name = name;
+            this.number = number;
+            this.rate = rate;
+            this.hours = hours;
+        }
+
+        /// <summary>
+        /// GetGross method which used in this class to calculate the total pay of the employee.
+        /// it just multiply the payrate with hours work and if the hours are more than 40 than it multiply the rate by 1.5
+        /// 
+        /// </summary>
+        /// <returns>gross</returns>
+        
+        public decimal GetGross()
+        {
+            if (hours <= 40)
+            {
+                gross = (decimal)hours * rate;
+            }
+            else
+            {
+                double overtimeHours = hours - 40; // it will give over time hours so it will count 1.5
+                decimal overtimeRate = (decimal)1.5 * rate;
+                decimal overtimePay = (decimal)overtimeHours * overtimeRate;
+
+                decimal normalPay = (decimal)40 * rate;
+
+                gross = overtimePay + normalPay;
+            }
+            return gross;
+        }      
+
+        /// <summary>
+        /// GetHours method which return the hours worked by employee.
+        /// </summary>
+        /// <returns>hours</returns>
+        public double GetHours()
+        {
+            return hours;
+        }
+
+        /// <summary>
+        /// GetName method which return the name of the employee.
+        /// </summary>
+        /// <returns>name</returns>
+        public string GetName()
+        {
+            return name;
+        }
+
+        /// <summary>
+        /// GetNumber method which return the number of the employee.
+        /// </summary>
+        /// <returns>number</returns>
+        public int GetNumber()
+        {
+            return number;
+        }
+
+        /// <summary>
+        /// GetRate method which return the pay rate of the employee.
+        /// </summary>
+        /// <returns>rate</returns>
+        public decimal GetRate()
+        {
+            return rate;
+        }
+
+        /// <summary>
+        /// SetHours method which set the hours of th eemployee.
+        /// </summary>
+        /// <param name="hours">Employees's worked hours</param>
+        public void SetHours(double hours)
+        {
+            this.hours = hours;
+
+        }
+
+        /// <summary>
+        /// SetName method which set the name of th eemployee.
+        /// </summary>
+        /// <param name="hours">Employees's name</param>
+        public void SetName(string name)
+        {
+            this.name = name;
+
+        }
+
+        /// <summary>
+        /// SetNumber method which set the number of th eemployee.
+        /// </summary>
+        /// <param name="hours">Employees's number</param>
+        public void SetNumber(int number)
+        {
+            this.number = number;
+        }
+
+        /// <summary>
+        /// Setrate method which set the pay rate of th eemployee.
+        /// </summary>
+        /// <param name="hours">Employees's pay rate</param>
+        public void setRate(decimal rate)
+        {
+            this.rate = rate;
+        }
+
+        /// <summary>
+        /// ToString method which return all variables name, number , rate, hours and method GetGross.
+        /// </summary>
+        /// <returns>name</returns>
+        /// <returns>number</returns>
+        /// <returns>rate</returns>
+        /// <returns>hours</returns>
+        /// <returns>GetGross</returns>
+        public override string ToString()
+        {
+            return $"{name,-15}{number,10}{"   "}{rate,0:c}{"   "}{hours,-6:n}{"   "}{GetGross(),-5:c}";
+
+        }
+
+
+    }
+}
---
+++ Sorting Application/Assignment1_000813104/Program.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+
+/// <summary>
+///  I, Abhay Panchal, 000813104 certify that this material is my original work.  
+///  No other person's work has been used without due acknowledgement.(SEP : 2020)
+/// </summary>
+
+namespace Assignment1_000813104
+{   
+    /// <summary>
+    /// Program class which has various methods which help application to execute properly.
+    /// It has Read method which read the data from Employee.csv file and also handle the exceptions.
+    /// Various SelectionSort methods which sort the data in many ways.
+    /// 
+    /// </summary>
+    class Program
+    {   
+        /// <summary>
+        /// Main method output data to console and take input from user.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Employee[] employees = Read("Employee.csv"); //Read() method with Employee array 
+
+            bool running = true;
+            while (running)//while loop
+            {
+                Console.WriteLine("[1] Sort by Employee Name: \n" +
+                                  "[2] Sort by Employee Number: \n" +
+                                  "[3] Sort by Employee Pay Rate: \n" +
+                                  "[4] Sort by Employee Hours: \n" +
+                                  "[5] Sort by Employee Gross Pay: \n\n" +
+                                  "[6] Exit: \n"); 
+
+                Console.WriteLine("ENTER OPTION: ");
+
+
+                try
+                {
+                    int input = int.Parse(Console.ReadLine());
+
+                    switch (input)
+                    {
+                        case 1:
+
+                            Console.WriteLine("Employee           Number   Rate     Hours   Gross Pay                Nick's Company");
+                            Console.WriteLine("==============     ======   =====    =====   =========                --------------");
+
+                            SelectionSortName(employees); // SelectionSortName method sort employees with names(ascending)
+                            foreach (Employee e in employees)
+                            {
+                                Console.WriteLine(e);
+                            }
+                            Console.WriteLine("\n");
+                            break;
+                        case 2:
+                            Console.WriteLine("Employee           Number   Rate     Hours   Gross Pay                Nick's Company");
+                            Console.WriteLine("==============     ======   =====    =====   =========                --------------");
+
+                            SelectionSortNumber(employees);// SelectionSortNumber method sort employees with number(ascending)
+                            foreach (Employee e in employees)
+                            {
+                                Console.WriteLine(e);
+                            }
+                            Console.WriteLine("\n");
+                            break;
+                        case 3:
+                            Console.WriteLine("Employee           Number   Rate     Hours   Gross Pay                Nick's Company");
+                            Console.WriteLine("==============     ======   =====    =====   =========                --------------");
+
+                            SelectionSortPayRate(employees);// SelectionSortPayRate method sort employees with rate(decending)
+                            foreach (Employee e in employees)
+                            {
+                                Console.WriteLine(e);
+                            }
+                            Console.WriteLine("\n");
+                            break;
+                        case 4:
+
+                            Console.WriteLine("Employee           Number   Rate     Hours   Gross Pay                Nick's Company");
+                            Console.WriteLine("==============     ======   =====    =====   =========                --------------");
+
+                            SelectionSortHours(employees);// SelectionSortHours method sort employees with hours(decending)
+                            foreach (Employee e in employees)
+                            {
+                                Console.WriteLine(e);
+                            }
+
+                            Console.WriteLine("\n");
+                            break;
+                        case 5:
+
+                            Console.WriteLine("Employee           Number   Rate     Hours   Gross Pay                Nick's Company");
+                            Console.WriteLine("==============     ======   =====    =====   =========                --------------");
+
+                            SelectionSortGross(employees);// SelectionSortGross method sort employees with grosspay(decending)
+                            foreach (Employee e in employees)
+                            {
+                                Console.WriteLine(e);
+                            }
+                            Console.WriteLine("\n");
+
+                            break;
+                        case 6:
+                            running = false;
+                            break;
+                    }
+                }
+                catch(Exception ex)
+                {
+                    Console.Error.WriteLine("*** INVALID CHOICE - TRY AGAIN ***\n\n");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Read method which read the data from text file
+        /// </summary>
+        /// <param name="filename">name of file </param>
+        /// <returns>null</returns>
+        public static Employee[] Read(string filename)
+        {
+            Employee[] employees = new Employee[100]; //Employee array 
+            int employeeCount = 0;
+
+            try
+            {
+                StreamReader reader = new StreamReader(filename);
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+                    string[] employeeName = line.Split(',');//split with (,)
+
+                    employees[employeeCount] = new Employee(employeeName[0], int.Parse(employeeName[1]), decimal.Parse(employeeName[2]), double.Parse(employeeName[3]));
+
+                    employeeCount++;
+                }
+                Array.Resize(ref employees, employeeCount);// truncate the array in correct size
+                return employees;
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex.Message);
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// SelectionSortName method which sort the data in acending order with name
+        /// In this method during first for loop it takes first value from array and the it enters to the second for loop and compare that first value 
+        /// with all value of the array during this if it find any value lower than the firstvalue it swap in between and the loop countinues and at the and 
+        /// the array get sorted according to the conditions (acending or decending)
+        /// 
+        /// Cite: https://www.tutorialspoint.com/selection-sort-program-in-chash
+        /// </summary>
+        /// <param name="employees"> Employees's name</param>
+        public static void SelectionSortName(Employee[] employees)
+        {
+            for (int i = 0; i < employees.Length - 1; i++)
+            {
+                int min = i;
+                for (int j = i + 1; j < employees.Length; j++)
+                {
+                    if (employees[j].GetName().ToLower().CompareTo(employees[min].GetName().ToLower()) < 0)
+                    {
+                        min = j;
+                        
+                    }
+                }
+                Employee temp = employees[i];
+                employees[i] = employees[min];
+                employees[min] = temp;
+            }
+        }
+        /// <summary>
+        /// Sort method which sort the data in acending order with numbers
+        /// </summary>
+        /// <param name="employees"> Employees' number</param>
+        public static void SelectionSortNumber(Employee[] employees)
+        {
+            for (int i = 0; i < employees.Length - 1; i++)
+            {
+                int min = i;
+                for (int j = i + 1; j < employees.Length; j++)
+                {
+                    if (employees[j].GetNumber().CompareTo(employees[min].GetNumber()) < 0)
+                    {
+                        min = j;
+
+                    }
+                }
+
+                Employee temp = employees[i];
+                employees[i] = employees[min];
+                employees[min] = temp;
+            }
+        }
+
+        /// <summary>
+        /// Sort method which sort the data in decending order with employee's pay rate 
+        /// </summary>
+        /// <param name="employees"> Employee's pay rate</param>
+        public static void SelectionSortPayRate(Employee[] employees)
+        {
+            for (int i = 0; i < employees.Length - 1; i++)
+            {
+                int min = i;
+                for (int j = i + 1; j < employees.Length; j++)
+                {
+                    if (employees[j].GetRate().CompareTo(employees[min].GetRate()) > 0)
+                    {
+                        min = j;
+
+                    }
+                }
+
+                Employee temp = employees[i];
+                employees[i] = employees[min];
+                employees[min] = temp;
+            }
+        }
+
+        /// <summary>
+        /// Sort method which sort the data in decending order with employee's working hours
+        /// </summary>
+        /// <param name="employees"> Employee's worked hours</param>
+        public static void SelectionSortHours(Employee[] employees)
+        {
+            for (int i = 0; i < employees.Length - 1; i++)
+            {
+                int min = i;
+                for (int j = i + 1; j < employees.Length; j++)
+                {
+                    if (employees[j].GetHours().CompareTo(employees[min].GetHours()) > 0)
+                    {
+                        min = j;
+
+                    }
+                }
+
+                Employee temp = employees[i];
+                employees[i] = employees[min];
+                employees[min] = temp;
+            }
+        }
+
+        /// <summary>
+        /// Sort method which sort the data in decending order with employee's gross pay 
+        /// </summary>
+        /// <param name="employees"> Employee's gross paye</param>
+
+        public static void SelectionSortGross(Employee[] employees)
+        {
+            for (int i = 0; i < employees.Length - 1; i++)
+            {
+                int min = i;
+                for (int j = i + 1; j < employees.Length; j++)
+                {
+                    if (employees[j].GetGross().CompareTo(employees[min].GetGross()) > 0)
+                    {
+                        min = j;
+
+                    }
+                }
+
+                Employee temp = employees[i];
+                employees[i] = employees[min];
+                employees[min] = temp;
+            }
+        }
+
+    }
+}
